e913377eb7ed14cd944d06f43326e440761c3d2d
Brian Haggerty <brian.haggery@epitech.eu>
2019-06-20 01:20:31 +0200

    'calculAjusted'

--- Controller/parcelController.js
+++ Controller/parcelController.js
@@ -1,3 +1,11 @@
+var num = 1;
+var orderToPack = [];
+const weight_max = 30;
+//const parcel_max = 15;
+//var parcel_weight = 0;
+//var parcel_number = 1;
+//var parcel = [];
+
 module.exports = {
     parcelTreatment: (treat) => {
         let itemsList = treat.items;
@@ -17,38 +25,25 @@ module.exports = {
     }
 };
 
-const weight_max = 30;
-const parcel_max = 15;
-var parcel_weight = 0;
-var parcel_number = 1;
-var parcel = [];
-var num = 1;
-var orderToPack = [];
-
 treatment = (order, itemsList) => {
     let id = order.id;
     let items = order.items;
+    let arr = [];
+    console.log(items);
 
     for (let i = 0; i < items.length; i++) {
         if (items[i] && typeof items[i].quantity !== 'undefined') {
-            var orderParcel = findWeigth(items[i], itemsList, id);
+            items.map(function (key, val) {
+                console.log('key: ' + JSON.stringify(key) + ' ' + JSON.stringify(val));
+                let orderParcel = findWeigth(key, itemsList, id);
+                arr.push(orderParcel);
+            });
+            return arr;
+           //throw new Error("");
         } else {
             throw new Error("Error: La quantités d'une commande n'est peut être pas définie. En savoir plus: " + items[i]);
         }
     }
-    return orderParcel;
-
-    let parcelToPack = new Object({
-        order_id: null,
-        items: [{
-            item_id: null,
-            quantity: null,
-        }],
-        unitWeight: null,
-        weight: null,
-        tracking_id: null,
-        palette_number: null,
-    });
 };
 
 /*
@@ -56,6 +51,7 @@ treatment = (order, itemsList) => {
 * la quantités d'objet si le poid excède 30kg
  */
 findWeigth = (itemOrder, itemsList, id) => {
+    console.log(itemOrder);
     let parcel = [];
     if (itemOrder && typeof itemOrder !== 'undefined' || itemOrder != null) {
         for (let i = 0; i < itemsList.length; i++) {
@@ -70,54 +66,59 @@ findWeigth = (itemOrder, itemsList, id) => {
                 let divide = null;
 
                 /* Indique la division les stacks trop lourds */
-                if (total > 30) {
+                /* Divise le stack si il doit être diviser */
+                /* Retourn un nouvelle objet avec l'id item et son poid + plus poids total selon quantités */
+
+                if (total > weight_max) {
                     if (quantity === 2) {
                         let divideBy = total - itemWeight;
                         let newWeight = Number(divideBy.toFixed(1));
                         quantity = 1;
                         divide = 2;
+                        for (let i = 0; i < divide; i++) {
+                            let parcelToStack = new Object({
+                                order_num: num,
+                                order_id: id,
+                                item_id: itemId,
+                                items: [{
+                                    item_id: itemId,
+                                    quantity: quantity,
+                                }],
+                                quantity: Number(quantity),
+                                unitWeight: Number(itemWeight),
+                                weight: Number(newWeight),
+                            });
+                            parcel.push(parcelToStack);
+                        }
                     } else {
                         let actualQuantity = quantity;
-                        let mem = 0;
                         for (let i = 0; i < 100; i++) {
                             let divideBy = total - itemWeight * i;
-                            let newWeight = Number(divideBy.toFixed(1));
+                            var newWeight = Number(divideBy.toFixed(1));
                             quantity--;
-                            if (divideBy < 30) {
+                            if (divideBy < weight_max) {
                                 divide = i;
-                                if(quantity === 0 && actualQuantity > 1){
+                                if (quantity === 0 && actualQuantity > 1) {
                                     quantity = 1;
                                 }
                                 break;
                             }
                         }
-                        //console.log('total: ' + total);
-                        //console.log('itemWeight: ' + itemWeight);
-                        //console.log('mem: ' + mem);
-                        //console.log('quantity: ' + quantity);
-                        //console.log('divide: ' + divide);
-                    }
-                }
-                /* Divise le stack si il doit être diviser */
-                /* Retourn un nouvelle objet avec l'id item et son poid + plus poids total selon quantités */
-                if (divide != null) {
-                    let divideItem = total - itemWeight * divide;
-                    let itemDivided = Number(divideItem.toFixed(1));
-                    console.log(divide, itemDivided, Number(itemWeight));
-                    for (let i = 0; i < divide + 1; i++) {
-                        let parcelToStack = new Object({
-                            order_num: num,
-                            order_id: id,
-                            item_id: itemId,
-                            items: [{
+                        for (let i = 0; i < divide + 1; i++) {
+                            let parcelToStack = new Object({
+                                order_num: num,
+                                order_id: id,
                                 item_id: itemId,
-                                quantity: quantity,
-                            }],
-                            quantity: Number(quantity),
-                            unitWeight: Number(itemWeight),
-                            weight: Number(itemDivided),
-                        });
-                        parcel.push(parcelToStack);
+                                items: [{
+                                    item_id: itemId,
+                                    quantity: quantity,
+                                }],
+                                quantity: Number(quantity),
+                                unitWeight: Number(itemWeight),
+                                weight: Number(newWeight),
+                            });
+                            parcel.push(parcelToStack);
+                        }
                     }
                 } else {
                     let weight = Number(itemWeight) * quantity;
@@ -136,7 +137,11 @@ findWeigth = (itemOrder, itemsList, id) => {
                     });
                     parcel.push(parcelToStack);
                 }
-                console.log(parcel);
+                //console.log(parcel);
+                //console.log('total: ' + total);
+                //console.log('itemWeight: ' + itemWeight);
+                //console.log('quantity: ' + quantity);
+                //console.log('divide: ' + divide);
                 return parcel;
             }
         }
@@ -145,24 +150,37 @@ findWeigth = (itemOrder, itemsList, id) => {
     }
 };
 
+
 AddStack = (order) => {
     let max = 30;
     let lenght = order.length;
-    let num = 0;
-    let remember = "";
-    console.log(lenght);
+    //console.log(lenght);
+    //console.log(JSON.stringify(order));
+    //throw new Error("Calcul");
 
     for (let i = 0; i < lenght; i++) {
-        order[i].map(function (key, value) {
-
-            //console.log(key, value);
+        //console.log(order[1]);
+        order[i].map(function (value, key) {
+            //console.log('key: ' + JSON.stringify(key) + ' ' + JSON.stringify(value[0].weight) + JSON.stringify(value));
+            console.log(JSON.stringify('val: '+ value));
+            value.forEach(function (element, key) {
+                console.log(key, element);
+            });
+            if (order[i] === order[1]) {
+                throw new Error("Calcul");
+            }
         });
-        if (order[i] === order[2]) {
-            throw new Error("Calcul");
-        }
     }
-};
 
-AddParcelToPalette = (parcel) => {
-
+    let parcelToPack = new Object({
+        order_id: null,
+        items: [{
+            item_id: null,
+            quantity: null,
+        }],
+        unitWeight: null,
+        weight: null,
+        tracking_id: null,
+        palette_number: null,
+    });
 };
