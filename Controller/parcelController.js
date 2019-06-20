d98a7b94ef024a0cb1dea0bb9ff8ee1e6a92153d
Brian Haggerty <brian.haggery@epitech.eu>
2019-06-20 06:18:05 +0200

    'addingAxios'

--- Controller/parcelController.js
+++ Controller/parcelController.js
@@ -1,21 +1,20 @@
-var num = 1;
-var orderToPack = [];
+const axios = require('axios');
 const weight_max = 30;
-//const parcel_max = 15;
-//var parcel_weight = 0;
-//var parcel_number = 1;
-//var parcel = [];
+const parcel_max = 15;
+var num = 1;
 
 module.exports = {
     parcelTreatment: (treat) => {
         let itemsList = treat.items;
         let ordersToCalcul = [];
+        let orderToPack = [];
         if (treat && typeof treat !== 'undefined') {
             treat.orders.forEach(function (order) {
                 let orderToPack = treatment(order, itemsList);
                 ordersToCalcul.push(orderToPack);
                 num++;
             });
+            //console.log(ordersToCalcul);
             AddStack(ordersToCalcul);
             return orderToPack
         } else {
@@ -29,17 +28,15 @@ treatment = (order, itemsList) => {
     let id = order.id;
     let items = order.items;
     let arr = [];
-    console.log(items);
 
     for (let i = 0; i < items.length; i++) {
         if (items[i] && typeof items[i].quantity !== 'undefined') {
-            items.map(function (key, val) {
-                console.log('key: ' + JSON.stringify(key) + ' ' + JSON.stringify(val));
-                let orderParcel = findWeigth(key, itemsList, id);
+            items.map(function (key) {
+                //console.log('key: ' + JSON.stringify(key) + ' ' + JSON.stringify(val));
+                let orderParcel = findWeigth(key, itemsList, id, order);
                 arr.push(orderParcel);
             });
             return arr;
-           //throw new Error("");
         } else {
             throw new Error("Error: La quantités d'une commande n'est peut être pas définie. En savoir plus: " + items[i]);
         }
@@ -49,9 +46,8 @@ treatment = (order, itemsList) => {
 /*
 * Fonction qui récupère le poids de l'objet unitaire, le poid total, divise
 * la quantités d'objet si le poid excède 30kg
- */
+*/
 findWeigth = (itemOrder, itemsList, id) => {
-    console.log(itemOrder);
     let parcel = [];
     if (itemOrder && typeof itemOrder !== 'undefined' || itemOrder != null) {
         for (let i = 0; i < itemsList.length; i++) {
@@ -137,11 +133,6 @@ findWeigth = (itemOrder, itemsList, id) => {
                     });
                     parcel.push(parcelToStack);
                 }
-                //console.log(parcel);
-                //console.log('total: ' + total);
-                //console.log('itemWeight: ' + itemWeight);
-                //console.log('quantity: ' + quantity);
-                //console.log('divide: ' + divide);
                 return parcel;
             }
         }
@@ -152,35 +143,131 @@ findWeigth = (itemOrder, itemsList, id) => {
 
 
 AddStack = (order) => {
-    let max = 30;
     let lenght = order.length;
-    //console.log(lenght);
-    //console.log(JSON.stringify(order));
-    //throw new Error("Calcul");
+    let parcelNumber = 1;
+    let paletteNumber = 1;
 
+    /* Premier Array = Commande, Second Array = Articles (divisé ou non)*/
+    //console.log(JSON.stringify(order));
+    //console.log(JSON.stringify(order[0][0][0]));
+    //console.log(JSON.stringify(order[0][1][0]));
+    //console.log(JSON.stringify(order[0][2][0]));
+    //throw new Error("Calcul");
+    console.log('Nombres de commandes: ' + lenght);
     for (let i = 0; i < lenght; i++) {
-        //console.log(order[1]);
+        let articleLenght = order[i].length;
+        console.log("------------------------------------");
+        console.log("Nombres d'articles: " + articleLenght);
+        console.log("Commande numéro: " + JSON.stringify(order[i][0][0].order_num));
+        for (let j = 0; j < articleLenght + 1; j++) {
+            if (articleLenght > 1) {
+                console.log("Commande : " + JSON.stringify(order[i][j][0]));
+                /* Commande avec plus d'un article, additionner pour maximiser le poids (30) (faire un sort ? )*/
+                let orderWeight = order[i][j][0].weight;
+                let next = j + 1;
+                let tryToAdd = order[i][next][0].weight;
+                if(j === 0 ){
+                    let parcelToPack = new Object({
+                        order_id: order[i][j][0].order_id,
+                        items: [{
+                            item_id: order[i][j][0].item_id,
+                            quantity: order[i][j][0].quantity,
+                        }],
+                        unitWeight: order[i][j][0].unitWeight,
+                        weight: order[i][j][0].weight,
+                        tracking_id: null,
+                        palette_number: paletteNumber,
+                    });
+                    parcelNumber++;
+                }
+                while (orderWeight < 30) {
+                    let addWeight = orderWeight + tryToAdd;
+                    console.log(next, addWeight);
+                    next += 1;
+                    if (addWeight >= 30 || typeof tryToAdd === 'undefined') {
+                        let parcelToPack = new Object({
+                            order_id: order[i][j][0].order_id,
+                            items: [{
+                                item_id: order[i][j][0].item_id,
+                                quantity: order[i][j][0].quantity,
+                            }],
+                            unitWeight: order[i][j][0].unitWeight,
+                            weight: order[i][j][0].weight,
+                            tracking_id: null,
+                            palette_number: paletteNumber,
+                        });
+                        parcelNumber++;
+                        break
+                    }else {
+                        tryToAdd = order[i][next][0].weight;
+                        console.log('poids qui vas être ajouté: ' + order[i][next][0].weight);
+                    }
+                }
+            } else {
+                console.log('un seul article dans la commande');
+                console.log(order[i][j][0]);
+                if (parcelNumber === 15) {
+                    paletteNumber++;
+                }
+                // Make a request for a user with a given ID
+                axios.get('https://helloacm.com/api/random/?n=15')
+                    .then(function (response) {
+                        // handle success
+                        console.log(response);
+                    })
+                    .catch(function (error) {
+                        // handle error
+                        console.log(error);
+                    })
+                    .finally(function () {
+                        // always executed
+                    });
+                let parcelToPack = new Object({
+                    order_id: order[i][j][0].order_id,
+                    items: [{
+                        item_id: order[i][j][0].item_id,
+                        quantity: order[i][j][0].quantity,
+                    }],
+                    unitWeight: order[i][j][0].unitWeight,
+                    weight: order[i][j][0].weight,
+                    tracking_id: null,
+                    palette_number: paletteNumber,
+                });
+                parcelNumber++;
+            }
+        }
+
+
+        /*if(order[i].length > 1){
+            // map
+        }
+        //console.log(order[0]);
         order[i].map(function (value, key) {
-            //console.log('key: ' + JSON.stringify(key) + ' ' + JSON.stringify(value[0].weight) + JSON.stringify(value));
-            console.log(JSON.stringify('val: '+ value));
-            value.forEach(function (element, key) {
-                console.log(key, element);
-            });
-            if (order[i] === order[1]) {
-                throw new Error("Calcul");
+            console.log('key: ' + JSON.stringify(key) + ' ' + JSON.stringify(value[i].weight) + JSON.stringify(value));
+            console.log(JSON.stringify('lenght: '+ value.length));
+            if(value[i].weight < 30){
+                console.log('smallerThan30');
             }
         });
-    }
 
-    let parcelToPack = new Object({
-        order_id: null,
-        items: [{
-            item_id: null,
-            quantity: null,
-        }],
-        unitWeight: null,
-        weight: null,
-        tracking_id: null,
-        palette_number: null,
-    });
+
+            for(let i = 0; i < value.length; i++){
+                console.log('value[i]: ' + JSON.stringify(value[i]));
+                console.log('i: ' + i);
+                let next = i += 1;
+                console.log('next: ' +next);
+            }
+
+            value.forEach(function (element, key) {
+                //console.log(key, element);
+            });
+
+
+            if (value[i] === value[0]) {
+                throw new Error("Calcul");
+            }
+
+
+         */
+    }
 };
