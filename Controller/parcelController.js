1d9b60554a4c1926241d02b2bc55235f63568c5e
Brian Haggerty <brian.haggery@epitech.eu>
2019-06-22 21:06:52 +0200

    (NeverGiveUp)

--- Controller/parcelController.js
+++ Controller/parcelController.js
@@ -8,7 +8,6 @@ var palette = [];
 
 module.exports = {
     parcelTreatment: (treat) => {
-        //palette = [];
         let itemsList = treat.items;
         let orders = treat.orders;
         let ordersToPack = [];
@@ -148,7 +147,7 @@ const getTrackId = async (url, order) => {
         const response = await axios.get(url);
         const data = response.data;
         if (data) {
-             return MakeParcel(order, data);
+            return MakeParcel(order, data);
         }
     } catch (error) {
         console.log(error);
@@ -156,69 +155,135 @@ const getTrackId = async (url, order) => {
 };
 
 StackAndPack = (order) => {
-    let newItems = [{}];
     let url = "https://helloacm.com/api/random/?n=15";
-    order.forEach(function (element) {
+    order.forEach(function (element, key) {
         let nbArticle = element.length;
         /* Si un seul article dans la commande, on fait directement le colis */
         /* TODO */
         /*FINISH HIM!*/
         if (nbArticle === 1) {
-            console.log('un seul article dans la commande');
+            //console.log('un seul article dans la commande');
             let packed = getTrackId(url, element);
             if (packed) {
-                console.log(packed);
-                //console.log('colis packed');
+                //console.log(packed);
             }
         }
         /* Si plusieurs Articles dans la commandes, on additionne pour remplir le maximum du poids autorisé (30 Kilos)*/
         /* TODO */
         if (nbArticle > 1) {
-            console.log(nbArticle + ' articles sont dans la commande');
-            let reach = 0;
-            element.forEach(function (value, key) {
-                //console.log(key + value);
+            //console.log(nbArticle + ' articles sont dans la commande');
+            let articleWeight = [];
+            element.forEach(function (value) {
+                let weight = {
+                    weight: JSON.stringify(value[0].weight),
+                    item_id: JSON.stringify(value[0].item_id),
+                    quantity: JSON.stringify(value[0].quantity),
+                    order_id: JSON.stringify(value[0].order_id),
+                };
+                articleWeight.push(weight);
+            });
+            let sorted = articleWeight.sort((a, b) => a.weight - b.weight);
+            sorted.reverse();
+            //console.log(sorted);
+            for (let i = 0; i < nbArticle; i++) {
+                if (typeof sorted[i + 1] == 'undefined') {
+                    break;
+                }
+                let addBigger = Number(sorted[i].weight) + Number(sorted[i + 1].weight);
+                if (addBigger.toFixed(1) <= 30) {
+                    sorted[i].weight = Number(addBigger.toFixed(1));
+                    let item1 = sorted[i].item_id;
+                    let item2 = sorted[i + 1].item_id;
+                    sorted[i].item_id = {
+                        item_id: {
+                            item1,
+                            item2
+                        },
+                    };
+                    let quantity1 = sorted[i].quantity;
+                    let quantity2 = sorted[i + 1].quantity;
+                    sorted[i].quantity = {
+                        quantity: {
+                            quantity1,
+                            quantity2
+                        },
+                    };
+                    sorted.splice(i + 1, 1);
+                } else {
+                    if (typeof sorted[i + 2] == 'undefined') {
+                        break;
+                    }
+                    let tryAddNext = Number(sorted[i].weight) + Number(sorted[i + 2].weight);
+                    //console.log(tryAddNext.toFixed(1));
+                    if (tryAddNext.toFixed(1) <= 30) {
+                        sorted[i].weight = Number(tryAddNext.toFixed(1));
+                        let item1 = sorted[i].item_id;
+                        let item2 = sorted[i + 2].item_id;
+                        sorted[i].item_id = {
+                            item_id: {
+                                item1,
+                                item2
+                            },
+                        };
+                        let quantity1 = sorted[i].quantity;
+                        let quantity2 = sorted[i + 2].quantity;
+                        sorted[i].quantity = {
+                            quantity: {
+                                quantity1,
+                                quantity2
+                            },
+                        };
+                        sorted.splice(i + 2, 1);
+                    } else {
 
+                    }
+                }
+            }
+            console.log(sorted);
+            sorted.forEach(function (value) {
+                console.log(value);
+                getTrackId(url, value);
             });
-            let parcelToPack = new Object({
-                order_id: null,
-                newItems,
-                weight: null,
-                tracking_id: null,
-                palette_number: nbPalette,
-            });
+            //console.log(sorted);
+            throw new Error("Pikachu is not dead yet");
         }
     });
     return palette;
 };
 
 /* TODO */
-/*FINISH HIM!*/
 MakeParcel = (order, track_id) => {
     let weight = order[0][0].weight;
-    if(nbParcel === parcel_max){
+    if (nbParcel === parcel_max) {
         nbPalette++;
         nbParcel = 0;
         console.log("nombre de colis = " + nbParcel + ', la nouvelle pallette est la numéro ' + nbPalette);
     }
     if (weight <= 30) {
+        let orderId = null;
+        let items = [{
+            item_id: null,
+            quantity: null
+        }];
+        let weight = null;
+        (typeof order[0][0].order_id == 'undefined') ? order.order_id = orderId : order[0][0].order_id = orderId;
+        (typeof order[0][0].item_id == 'undefined') ? order.item_id.item_id = items['item_id'] : order[0][0].item_id = items['item_id'];
+        (typeof order[0][0].quantity == 'undefined') ? order.quantity.quantity = items['quantity'] : order[0][0].quantity = items['quantity'];
+        (typeof order[0][0].weight == 'undefined') ? order.weight = weight : order[0][0].weight = weight;
+
         let parcel = new Object({
-            order_id: order[0][0].order_id,
-            items: [{
-                item_id: order[0][0].item_id,
-                quantity: order[0][0].quantity,
-            }],
-            weight: order[0][0].weight,
+            order_id: orderId,
+            items,
+            weight: weight,
             tracking_id: track_id,
             palette_number: nbPalette,
         });
         nbParcel++;
         Remuneration(parcel.weight);
         palette.push(parcel);
-        //console.log(palette);
-/*      console.log(palette);
+        console.log(palette);
         console.log(parcel);
-        console.log('**********');*/
+        console.log('**********');
         return palette;
     } else {
         throw new Error("Pikachu is not dead yet");
@@ -226,7 +291,6 @@ MakeParcel = (order, track_id) => {
 };
 
 /* TODO */
-/*FINISH HIM!*/
 Remuneration = (poids) => {
     //console.log(poids);
 };
